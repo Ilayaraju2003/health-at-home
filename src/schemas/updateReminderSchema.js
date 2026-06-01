143de889c5bac15bada2983b61469c3b9ec1e130
Ilayaraju2003 <ilayaraju2003@gmail.com>
2026-06-01 13:57:53 +0530

    backend commit

--- src/schemas/updateReminderSchema.js
+++
@@ -1,5 +0,0 @@
-export const updateReminderSchema = z.object({
-  reminder: z.string().min(1, 'Reminder should be longer').max(255).optional(),
-  notes: z.string().nullable().optional(),
-  completed: z.boolean().optional(),
-});
